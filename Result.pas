2ee330b7dc388d3c902ae655dae65ec2144a86c7
Stukalova Kristina <kristy.nelsy@gmail.com>
2016-02-14 02:19:49 +0600

    0.3

--- Result.pas
+++ Result.pas
@@ -27,8 +27,10 @@ procedure TFormResult.FormPaint(Sender: TObject);
 var i:Integer;
 begin
   FormDBUtils.ADOQuery.SQL.Clear;
-  FormDBUtils.ADOQuery.SQL.Add('SELECT type_id, count FROM user_data Where user_id=1');
+  FormDBUtils.ADOQuery.SQL.Add('SELECT type_desc AS Тип, count AS Показатель FROM user_data LEFT JOIN types ON user_data.type_id = types.type_id WHERE user_id=1;');
   FormDBUtils.ADOQuery.Active:=True;
+
+  DBGrid1.Columns[0].Width:=50;
 end;
 
 end.
